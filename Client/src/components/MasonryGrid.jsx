ef79873c5c58f1961becf16e0009e5fc354db880
Tanner Davison <tanner.davison95@gmail.com>
2024-06-18 23:33:01 -0600

    added chunking

--- Client/src/components/MasonryGrid.jsx
+++ Client/src/components/MasonryGrid.jsx
@@ -1,119 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import media from "styles/media";
 import colors from "styles/colors";
 import text from "styles/text";
 
 const MasonryGrid = () => {
+  const [imgChunks, setImageChunks] = useState([]);
+  const img1 =
+    "https://cdna.artstation.com/p/marketplace/presentation_assets/001/774/626/large/file.jpg?1654797523";
+  const img2 =
+    "https://c4.wallpaperflare.com/wallpaper/995/599/664/art-friendship-trust-baby-children-s-hd-wallpaper-preview.jpg";
+  const img3 =
+    "https://c4.wallpaperflare.com/wallpaper/995/599/664/art-friendship-trust-baby-children-s-hd-wallpaper-preview.jpg";
+  const photosArray = [img1, img2, img3, img2, img3, img1, img3, img1, img2];
+  const chunkSize = 3;
+
+  useEffect(() => {
+    let chunks = [];
+    for (let i = 0; i < photosArray.length; i += chunkSize) {
+      chunks.push(photosArray.slice(i, i + chunkSize));
+    }
+    setImageChunks(chunks);
+  }, []);
+
+  const renderChunks = imgChunks.map((chunk, index) => {
+    return (
+      <Column key={index + chunk + 2}>
+        {chunk.map((imgUrl, index) => {
+          return (
+            <Photo key={index + imgUrl}>
+              <Image src={imgUrl} alt={"random-image"} />
+            </Photo>
+          );
+        })}
+      </Column>
+    );
+  });
+
   return (
     <Wrapper>
       <h2>Photo Gallery</h2>
-      <PhotoGallery>
-        <Column>
-          <Photo>
-            <Image
-              src={
-                "https://cdna.artstation.com/p/marketplace/presentation_assets/001/774/626/large/file.jpg?1654797523"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://c4.wallpaperflare.com/wallpaper/995/599/664/art-friendship-trust-baby-children-s-hd-wallpaper-preview.jpg"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://e0.pxfuel.com/wallpapers/126/740/desktop-wallpaper-modern-random-b-scb-modern-earth.jpg"
-              }
-              alt="random photo"
-            />
-          </Photo>
-        </Column>
-        <Column>
-          <Photo>
-            <Image
-              src={
-                "https://www.gamewallpapers.com/img_script/wallpaper_dir/img.php?src=wallpaper_lost_in_random_02_2560x1440.jpg&height=450&width=800&fill-to-fit&sharpen"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://c4.wallpaperflare.com/wallpaper/995/599/664/art-friendship-trust-baby-children-s-hd-wallpaper-preview.jpg"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://e0.pxfuel.com/wallpapers/126/740/desktop-wallpaper-modern-random-b-scb-modern-earth.jpg"
-              }
-              alt="random photo"
-            />
-          </Photo>
-        </Column>
-        <Column>
-          <Photo>
-            <Image
-              src={
-                "https://www.gamewallpapers.com/img_script/wallpaper_dir/img.php?src=wallpaper_lost_in_random_02_2560x1440.jpg&height=450&width=800&fill-to-fit&sharpen"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://cdna.artstation.com/p/marketplace/presentation_assets/001/774/626/large/file.jpg?1654797523"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://e0.pxfuel.com/wallpapers/126/740/desktop-wallpaper-modern-random-b-scb-modern-earth.jpg"
-              }
-              alt="random photo"
-            />
-          </Photo>
-        </Column>
-        <Column>
-          <Photo>
-            <Image
-              src={
-                "https://www.gamewallpapers.com/img_script/wallpaper_dir/img.php?src=wallpaper_lost_in_random_02_2560x1440.jpg&height=450&width=800&fill-to-fit&sharpen"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://c4.wallpaperflare.com/wallpaper/995/599/664/art-friendship-trust-baby-children-s-hd-wallpaper-preview.jpg"
-              }
-              alt="random photo"
-            />
-          </Photo>
-          <Photo>
-            <Image
-              src={
-                "https://e0.pxfuel.com/wallpapers/126/740/desktop-wallpaper-modern-random-b-scb-modern-earth.jpg"
-              }
-              alt="random photo"
-            />
-          </Photo>
-        </Column>
-      </PhotoGallery>
+      <PhotoGallery>{renderChunks}</PhotoGallery>
     </Wrapper>
   );
 };
@@ -123,12 +50,10 @@ const Image = styled.img`
   width: 100%;
   height: 100%;
   border-radius: 5px;
-object-fit: cover;
-  flex:2;
+  object-fit: cover;
+  flex: 2;
 `;
-const Photo = styled.div`
-
-`;
+const Photo = styled.div``;
 const Column = styled.div`
   display: flex;
   flex-direction: column;
@@ -138,15 +63,13 @@ const PhotoGallery = styled.div`
   display: flex;
   gap: 20px;
   ${media.fullWidth} {
-  
   }
-  
+
   ${media.tablet} {
-  
   }
-  
+
   ${media.mobile} {
-  flex-direction: column;
+    flex-direction: column;
   }
 `;
 const Wrapper = styled.div`
